e62b13d3b7723c0d76fb695d7860c2fb28e897a1
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-06-18 12:54:41 +0200

    finished task2

---
+++ task2/app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_json('people.json')
+
+# Keep only people aged 30 or younger
+filtered_by_age = df[df['age'] <= 30]
+
+grouped_by_city = filtered_by_age.sort_values(by=['city'])
+
+grouped_by_city.to_csv('filtered_grouped.csv', index=False)
+
+print('Filtered and grouped data saved to filtered_grouped.csv')
