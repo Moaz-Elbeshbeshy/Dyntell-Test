d04843646842c2e616d40512e520124b4c261e4f
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-06-22 22:38:15 +0200

    added Terraform files to create vm on GCP

---
+++ task3/infra/main.tf
@@ -0,0 +1,63 @@
+resource "google_compute_address" "vm_static_ip" {
+    name     = "php-vm-static-ip"
+    region   = var.region
+}
+
+
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = ["task-manager"]
+
+  depends_on = [ google_compute_address.vm_static_ip ]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+            nat_ip = google_compute_address.vm_static_ip.address
+        }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+    sudo usermod -aG docker $USER
+  EOT
+}
+
+resource "google_compute_firewall" "allow-frontend-backend" {
+  name    = "allow-frontend-backend"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000", "5000"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+  target_tags = ["task-manager"]
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["37.76.40.225/32"]  # ✅ Only your IP can access it
+
+  target_tags = ["task-manager"]        # ✅ Again, matches VM tag
+}
---
+++ task3/infra/variables.tf
@@ -0,0 +1,26 @@
+variable "project_id" {
+    type        = string
+    description = "GCP task-manager ID"
+}
+
+variable "region" {
+    type    =  string
+    default = "us-central1"
+}
+
+variable "zone" {
+    type      = string
+    default   = "us-central1-a"
+}
+
+variable "vm_name" {
+  default = "task-manager-vm"
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "machine_type" {
+  default = "e2-micro"
+}
