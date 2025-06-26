b6784277ffbc9058c6ffa3a8d1b6725653f44e57
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-06-26 21:59:33 +0200

    fixed k3s installation to fit with amazon-linux

--- task3/infra_aws/scripts/setup.sh
+++ task3/infra_aws/scripts/setup.sh
@@ -8,6 +8,11 @@ echo "=== Starting setup script ==="
 # Update system packages
 sudo yum update -y
 
+# Disable SELinux (Amazon Linux 2 has incompatible versions with k3s-selinux)
+echo "Disabling SELinux..."
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+
 # Install Docker from Amazon Linux Extras
 sudo amazon-linux-extras install docker -y
 
@@ -18,9 +23,10 @@ sudo systemctl enable docker
 # Add ec2-user to the docker group so you can run docker without sudo
 sudo usermod -aG docker ec2-user
 
-# Install K3s (lightweight Kubernetes)
+# Install K3s (lightweight Kubernetes) with SELinux disabled
 if ! command -v k3s &> /dev/null; then
-  curl -sfL https://get.k3s.io | sh -
+  echo "Installing K3s..."
+  curl -sfL https://get.k3s.io | INSTALL_K3S_SELINUX_SUPPORT=false sh -
 fi
 
 # Alias kubectl to k3s kubectl for ease of use
