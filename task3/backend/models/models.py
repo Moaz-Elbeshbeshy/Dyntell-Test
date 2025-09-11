0fdc37dc18c985e5e5a2eb540bdbd8e3c8ccc6c2
Moaz Elbeshbeshy <awladnasem800@gmail.com>
2025-09-11 20:29:40 +0200

    added column to the table

--- task3/backend/models/models.py
+++ task3/backend/models/models.py
@@ -11,6 +11,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     status = db.Column(db.String(20), default="pending")
+    urgencey = db.Column(db.Integer, default=1)
     created_at = db.Column(
         db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc)
     )
